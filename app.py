9dafd6822f71de5b9dd3267a1528f5932d09d852
joshuadesmith <joshuadesmith@utexas.edu>
2018-10-01 13:36:21 -0500

    set up super basic graphql endpoint

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+
+from schema import schema
+
 app = Flask(__name__)
+app.debug = True
+
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def index():
+    return 'Go to /graphql'
 
 
 if __name__ == '__main__':
-    app.run(port=8088)
+    app.run()
--- schema.py
+++ schema.py
@@ -1,1 +1,17 @@
 # Definitions of GraphQL object types, queries, mutations, and schema
+import graphene
+
+
+class UserObject(graphene.ObjectType):
+    user_email = graphene.String()
+    spotify_auth_code = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    user_by_email = graphene.Field(graphene.String, email=graphene.String())
+
+    def resolve_user_by_email(self, info, **kwargs):
+        return f"Test {kwargs.get('email')}"
+
+
+schema = graphene.Schema(query=Query)
