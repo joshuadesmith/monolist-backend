54374cc57411153fbd2c3632f3520251fdbe39db
joshuadesmith <joshuadesmith@utexas.edu>
2018-10-02 13:29:32 -0500

    fix ID generation

--- api/dal/util.py
+++ api/dal/util.py
@@ -1,8 +1,18 @@
 # Helper methods for getting ID strings for DynamoDB Primary keys
+import base64
+import hashlib
+
+
 class NodeIDGenerator:
     @staticmethod
     def get_user_node_id(email):
-        return f"user-{str(hash(email))}"
+        return f"user-{NodeIDGenerator.generate_hashed_id(email)}"
+
+    @staticmethod
+    def generate_hashed_id(s):
+        encoded = s.encode('utf-8')
+        b64 = base64.b64encode(hashlib.md5(encoded).digest())
+        return str(b64)
 
 
 def convert_dict_for_dynamo(d: dict) -> dict:
